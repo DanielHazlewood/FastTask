2062a39497ed2d3300e1a9df722c31c33988abe5
Daniel Hazlewood <34265576+DanielHazlewood@users.noreply.github.com>
2020-12-04 14:23:49 +0400

    Renamed DBJob to JobDb..since it's cleaner.

--- src/FastTask.Core/Models/JobDb.cs
+++ src/FastTask.Core/Models/JobDb.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace FastTask.Core.Models
 {
-    public class DBJob
+    public class JobDb
     {
         [BsonId]
         public ObjectId Id { get; set; }
